a1e6be423f6371d1893d90d1716c21bab6150d30
OK@itproto <itprotoltd@gmail.com>
2026-02-05 18:12:52 +0000

    Ff

--- Foo.tsx
+++ Foo.tsx
@@ -1,5 +1,35 @@
-type ValueOrFn<T, R> = R | ((arg: T) => R);
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import type { RootState } from "../store";
 
-function resolveValue<T, R>(value: ValueOrFn<T, R>, arg: T): R {
-  return typeof value === "function" ? value(arg) : value;
-}
+/**
+ * High-level intent:
+ * “A currency pair was selected, keep family + pair consistent”
+ */
+export const selectPairAndSyncFamily = createAsyncThunk<
+  void,
+  CcyPair,
+  { state: RootState }
+>(
+  "ccyFamilies/selectPairAndSyncFamily",
+  (pair, { getState, dispatch }) => {
+    const state = getState().ccyFamilies;
+
+    const { ccyFamilies, selectedCcyFamilyName } = state;
+
+    const currentFamily =
+      ccyFamilies.find(f => f.name === selectedCcyFamilyName);
+
+    // If no family selected yet, or current family doesn't contain the pair
+    if (!currentFamily || !currentFamily.pairs.includes(pair)) {
+      const targetFamily = ccyFamilies.find(f =>
+        f.pairs.includes(pair)
+      );
+
+      if (targetFamily) {
+        dispatch(selectCcyFamily(targetFamily));
+      }
+    }
+
+    dispatch(selectCcyPair(pair));
+  }
+);
